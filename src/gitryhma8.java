219894e512ca60725441e3c5bb7157d95cf93e94
Tiia-Tuulia Vänttilä <amk1005402@student.hamk.fi>
2024-11-20 13:52:25 +0200

    Lisätty koodia
    Terkuin Tiia

--- src/gitryhma8.java
+++ src/gitryhma8.java
@@ -1,5 +1,6 @@
 public class gitryhma8 {
     public static void main(String[] args) throws Exception {
         System.out.println("Heipä hei");
+        System.out.println("Heippa minunki puolesta")
     }
 }
